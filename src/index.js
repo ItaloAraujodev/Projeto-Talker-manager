cf47e9898347d76e9225fe2a71e4fc4a91d5c6c5
italo21211 <italoarauju2019@gmail.com>
2022-10-04 19:43:41 -0300

    Desafio 2 completo

--- src/index.js
+++ src/index.js
@@ -9,6 +9,19 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const verify = async (req, res, next) => {
+  const talker = await readTalkerFile();
+  const { id } = req.params;
+  const pegandoId = talker.some((element) => element.id === Number(id));
+
+  if(!pegandoId){
+    return res.status(404).json({
+      "message": "Pessoa palestrante não encontrada"
+    });
+  }
+  next();
+};
+
 // não remova esse endpoint, e para o avaliador funcionar.
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -18,7 +31,16 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 
+/* -----------  INICIANDO ------------- */
+
 app.get('/talker',  async (_request, response) => {
   const talker = await readTalkerFile();
   return response.status(200).json(talker);
+});
+
+app.get('/talker/:id', verify,  async (req, res) => {
+    const talker = await readTalkerFile();
+    const { id } = req.params;
+    const pegandoId = talker.find(({id}) => id === Number(id));
+    return res.status(200).json(pegandoId)
 });
